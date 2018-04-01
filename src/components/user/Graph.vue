0094b78f5fab896fd840d5ee355846788c9bf781
Marko Rusic <markousp5@gmail.com>
2018-04-01 14:31:28 +0200

    link imgs

--- src/components/user/Graph.vue
+++ src/components/user/Graph.vue
@@ -1,23 +1,24 @@
 <template>
-  <!-- <div class='tableauPlaceholder' id='viz1522555079548' style='position: relative'><noscript><a href='#'><img alt='Mobilni internet ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ne&#47;net_potrosnja&#47;Mobilniinternet&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='net_potrosnja&#47;Mobilniinternet' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;ne&#47;net_potrosnja&#47;Mobilniinternet&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='filter' value='publish=yes' /></object></div>                 -->
-  <!-- <div class='tableauPlaceholder' id='viz1522554996774' style='position: relative'><noscript><a href='#'><img alt='SMS ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;8M&#47;8M32PJ8SC&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='path' value='shared&#47;8M32PJ8SC' /> <param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;8M&#47;8M32PJ8SC&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='filter' value='publish=yes' /></object></div> -->
-  <!-- <div>
-      <img src="https://photos-3.dropbox.com/t/2/AACP9wAeqlLwonkqLs_dIyUBbAwR5Kk3VILsqlkDAkL0TA/12/272823072/png/32x32/3/1522584000/0/2/Minuti.png/EPn8hYMCGKYGIAIoAg/MKOEu5IoWTM6sCDrLaXjvtoS4pkyRC1KOhRM0vg_1OA?dl=0&preserve_transparency=1&size=2048x1536&size_mode=3" alt="">
-  </div> -->
     <b-tabs>
         <b-tab title="Minuti" active>
             <div>
-                <img src="https://photos-3.dropbox.com/t/2/AAAK3sLEcuo7E84D0tVKqiSyOnyoWRIMfBDi67BXJmh3MQ/12/272823072/png/32x32/3/1522602000/0/2/Minuti.png/EPn8hYMCGKoGIAIoAg/nisoRGOFyz-2f6ugj9XmpbG9tLzTjjkpmzJJsWiREmk?dl=0&preserve_transparency=1&size=2048x1536&size_mode=3" alt="">
+                <a href="http://localhost/minutes.html" target="_blank">
+                    <img src="https://photos-3.dropbox.com/t/2/AAAK3sLEcuo7E84D0tVKqiSyOnyoWRIMfBDi67BXJmh3MQ/12/272823072/png/32x32/3/1522602000/0/2/Minuti.png/EPn8hYMCGKoGIAIoAg/nisoRGOFyz-2f6ugj9XmpbG9tLzTjjkpmzJJsWiREmk?dl=0&preserve_transparency=1&size=2048x1536&size_mode=3" alt="">
+                </a>
             </div>
         </b-tab>
         <b-tab title="Poruke" >
             <div>
-                <img src="https://photos-2.dropbox.com/t/2/AADMrehUtRzJBDoZuC6YtabKcEzRgyRwSgqpnC03HokwXw/12/272823072/png/32x32/3/1522602000/0/2/SMS.png/EPn8hYMCGKoGIAIoAg/uyWxW5byG262yVgJVWMaD5sY54f1CWdWZzSfzIIU4mY?dl=0&preserve_transparency=1&size=2048x1536&size_mode=3" alt="">
+                <a href="http://localhost/sms.html" target="_blank">
+                    <img src="https://photos-2.dropbox.com/t/2/AADMrehUtRzJBDoZuC6YtabKcEzRgyRwSgqpnC03HokwXw/12/272823072/png/32x32/3/1522602000/0/2/SMS.png/EPn8hYMCGKoGIAIoAg/uyWxW5byG262yVgJVWMaD5sY54f1CWdWZzSfzIIU4mY?dl=0&preserve_transparency=1&size=2048x1536&size_mode=3" alt="">
+                </a>
             </div>
         </b-tab>
         <b-tab title="Internet">
             <div>
-                <img src="https://photos-3.dropbox.com/t/2/AACHbpKTcbl_M33-OBl-oH0jA-ueCgoH9VH_c13ApGenQw/12/272823072/png/32x32/3/1522602000/0/2/Mobilni%20internet.png/EPn8hYMCGKoGIAIoAg/UgDf4XrLoL8w60YfU_2RAyT73Akvw7wnQItqbldvbiA?dl=0&preserve_transparency=1&size=2048x1536&size_mode=3" alt="">
+                <a href="http://localhost/net.html" target="_blank">
+                    <img src="https://photos-3.dropbox.com/t/2/AACHbpKTcbl_M33-OBl-oH0jA-ueCgoH9VH_c13ApGenQw/12/272823072/png/32x32/3/1522602000/0/2/Mobilni%20internet.png/EPn8hYMCGKoGIAIoAg/UgDf4XrLoL8w60YfU_2RAyT73Akvw7wnQItqbldvbiA?dl=0&preserve_transparency=1&size=2048x1536&size_mode=3" alt="">
+                </a>
             </div>
         </b-tab>
     </b-tabs>
@@ -25,16 +26,7 @@
 
 <script>
 export default {
-  mounted () {
-    //   var divElement = document.getElementById('viz1522555079548')
-    //   var vizElement = divElement.getElementsByTagName('object')[0]
-    //   vizElement.style.width='100%'
-    //   vizElement.style.height=(divElement.offsetWidth*0.75)+'px'
-    //   var scriptElement = document.createElement('script') 
-    //   scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'
-    //   vizElement.parentNode.insertBefore(scriptElement, vizElement)
-    // var divElement = document.getElementById('viz1522554996774');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);
-  }
+
 }
 </script>
 
